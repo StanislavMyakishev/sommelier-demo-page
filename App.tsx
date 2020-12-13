23bf4e37aa2a42b089487831947f2c43a9f40739
StanislavMyakishev <masiff-s@live.ru>
2020-12-14 02:30:30 +0300

    added gitignore updated page

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import { ThemeProvider } from "styled-components/native";
 import {theme, Box} from "sommelier";
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
-            <View>
-                <Box>
-                    <Text>sommelier is working!</Text>
-                </Box>
-            </View>
+            <Box backgroundColor="lightGray">HEE HEE</Box>
         </ThemeProvider>
     );
 };
