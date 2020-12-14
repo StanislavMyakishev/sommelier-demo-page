bb570bedf2f8485525753ce8ca6f9239b4e4d287
m.pavlov <m.pavlov@okko.tv>
2020-12-14 16:35:05 +0300

    latest sommelier 0.4.6

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { ThemeProvider } from "styled-components/native";
-import {theme, Box} from "sommelier";
+import {
+    Box, BoxProps,
+    Button, ButtonProps,
+    Checkbox, CheckboxProps,
+    Image, ImageProps,
+    InputPrice, InputPriceProps,
+    Navbar, Item, NavbarProps, NavbarItemProps,
+    RadioButton, RadioButtonProps,
+    SearchInput, SearchInputProps,
+    StarsRating, StarsRatingProps,
+    Switch, SwitchProps,
+    H1, H2, H3, H4, H5, H6,
+    Header, HeaderProps,
+    Text, TextProps,
+    TypographyProps,
+    theme
+} from "sommelier";
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Box backgroundColor="lightGray">HEE HEE</Box>
+            <Box backgroundColor="lightGray">
+                <Header size='h1'>COLOR</Header>
+            </Box>
         </ThemeProvider>
     );
 };
